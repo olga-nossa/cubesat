4258f328d25235b4816050886949b6aa8f9339b9
olga nossa <olga.nossa@udea.edu.co>
2024-01-25 20:49:43 -0500

    commit

--- anemometro/mpx5010dp-solo/mpx5010dp-solo.ino
+++ anemometro/mpx5010dp-solo/mpx5010dp-solo.ino
@@ -1,7 +1,25 @@
-void setup() {
+/*El código de este archivo tendrá como objetivo calibrar el sensor MPX5010DP (ajustar la función de transferencia).
+  Este código SÓLAMENTE se puede probar en un ARDUINO.
+*/
 
+double Vout,P,Vs = 5.0;
+double aux;
+double tolP = 0.04; // Este valor se debe determinar
+int i;
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
- 
- }
+void loop() 
+{
+   aux=0;
+   for(i=0;i<10;i++){
+    aux = aux + (float(analogRead(A0))*5.0/1023.0); //cálculo del voltaje
+    delay(5);
+   }
+  Vout=aux/10.0;
+
+  P = ( Vout - 0.04*Vs ) / (0.09 * Vs) + tolP; //Valor de la diferencia de presión usando la función de transferencia.
+}
