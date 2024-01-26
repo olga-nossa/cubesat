fccd5388a819e5ebcd261a9d9526a8ebea3280b8
olga nossa <olga.nossa@udea.edu.co>
2024-01-26 12:38:50 -0500

    commit

--- anemometro/ads1115-solo/ads1115-solo.ino
+++ anemometro/ads1115-solo/ads1115-solo.ino
@@ -19,11 +19,13 @@ void loop(void)
   int16_t adc0, adc1, adc2, adc3; // se declaran como variables que representan números enteros de 16 bits con signo
   float volts0, volts1, volts2, volts3;
 
+  // esta función lee la señal analógica
   adc0 = ads.readADC_SingleEnded(0);
   adc1 = ads.readADC_SingleEnded(1);
   adc2 = ads.readADC_SingleEnded(2);
   adc3 = ads.readADC_SingleEnded(3);
 
+  // esta función calcula el valor del voltaje de la señal analógica
   volts0 = ads.computeVolts(adc0);
   volts1 = ads.computeVolts(adc1);
   volts2 = ads.computeVolts(adc2);
